1a6ee5d34f105bd1175cd3b0dbf6501c355c95bc
dopeldev <322kuroneko@gmail.com>
2023-12-30 18:36:08 -0500

    fracaso al hacer hot reload. Comenzado nuevamente.

---
+++ about/static/vue/compenents/Header.vue
@@ -0,0 +1,34 @@
+<template>
+	<section class="container">
+		<div class="columns color4">
+			<div class="column is-2">
+				<figure class="image is-128x128">
+					<img class="is-rounded" src="{{ url_for('static', filename='img/pic128.webp') }}" alt="Profile picture of Pedro 'dopeldev' Gonzales">
+				</figure>
+			</div>
+			<div class="column auto">
+				<h1 class="title is-2">Pedro GG "Dopeldev" Gonzales</h1>
+				<p class="subtitle is-4">Software Developer</p>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+	export default {
+		name: 'Header',
+		data() {
+			return {
+				title: 'Header'
+			}
+		},
+		props: {
+			img: String,
+		},
+	}
+</script>
+
+<style scoped>
+
+</style>
+
