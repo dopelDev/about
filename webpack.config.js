8ce738bbac25329b0eef7433baa87c08c4c9dc88
dopeldev <322kuroneko@gmail.com>
2024-01-02 16:55:36 -0500

    Integration works

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
-const { VueLoaderPlugin } = require('vue-loader');
-const path = require('path');
+const path = require('node:path');
+const {VueLoaderPlugin} = require('vue-loader');
 
 module.exports = {
-  entry: './about/static/js/app.js', // Punto de entrada de tu aplicación
-  output: {
-    path: path.resolve(__dirname, './about/static/'), // Directorio de salida
-    filename: './js/app.bundle.js' // Nombre del archivo empaquetado
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      // Aquí puedes añadir más reglas para otros tipos de archivos
-    ]
-  },
-  plugins: [
-    // Asegúrate de incluir el plugin de Vue Loader
-    new VueLoaderPlugin()
-  ],
+	entry: './about/static/js/app.js', // Punto de entrada de tu aplicación
+	output: {
+		path: path.resolve(__dirname, './about/static/'), // Directorio de salida
+		filename: './js/app.bundle.js', // Nombre del archivo empaquetado
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+			},
+			// Aquí puedes añadir más reglas para otros tipos de archivos
+		],
+	},
+	plugins: [
+		// Asegúrate de incluir el plugin de Vue Loader
+		new VueLoaderPlugin(),
+	],
 	resolve: {
 		extensions: ['.js', '.vue', '.json'],
 		alias: {
-			'vue$': './node_modules/vue/dist/vue.esm.js',
+			vue$: path.resolve(__dirname, 'node_modules/vue/dist/vue.esm-bundler.js'),
+		},
+	},
 };
-
