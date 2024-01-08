fa0b58f10b43f84c068f2c32ca980086ed6233ce
dopeldev <322kuroneko@gmail.com>
2024-01-08 01:10:50 -0500

    Release 1.1

--- about/static/js/app.js
+++ about/static/js/app.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import Header from "../vue/components/Header.vue";
 import VueAlert from "../vue/components/VueAlert.vue";
 import SocialMedia from "../vue/components/SocialMedia.vue";
+import Home from '../vue/components/Home.vue';
 
 createApp(Header).mount('#header');
 createApp(VueAlert).mount('#vue-alert');
 createApp(SocialMedia).mount('#social-media');
+createApp(Home).mount('#home-alert');
 
---
+++ about/static/vue/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="block">
+		<div @click="open = true">home</div>
+		<Teleport to="body">
+		<div v-if="open" class="modal">
+			<div class="message is-info">
+				<div class="message-header">
+					<h1 class="has-text-centered">申し訳ございません</h1>
+				</div>
+				<div @click="open = false" class="message-body">
+					<p>This page is in development</p>
+				</div>
+			</div>
+		</div>
+		</Teleport>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Home',
+		data() {
+			return {
+				open: false
+			}
+		},
+	}
+</script>
+
+<style scoped>
+.modal {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	z-index: 999;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.close-button {
+	background-color: red;
+}
+</style>
+
