cc825ea2c85ad8eb152d465d6be7f9fd20e61c29
dopeldev <322kuroneko@gmail.com>
2024-01-11 19:53:04 -0500

    fix form id name

--- about/static/js/custom.js
+++ about/static/js/custom.js
@@ -1,4 +1,4 @@
-document.getElementById('scroll-to-form').addEventListener('click', scroll_to_form);
+document.getElementById('form').addEventListener('click', scroll_to_form);
 function scroll_to_form() {
 	const div = document.getElementById('form');
 	div.scrollIntoView({behavior: 'smooth'});
